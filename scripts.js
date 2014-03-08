9f63c06d3186b7b8ace8074184c78006b717dabd
Rishi <ri2195@columbia.edu>
2014-03-07 21:52:36 -0500

    Problem with mHeader

--- scripts.js
+++ scripts.js
@@ -15,87 +15,56 @@
 */
 
 
-
-function stepThree(DATASET){
-	console.log(DATASET);
+// 3) Since I am using the GDP dataset instead of UNEMP, I will name my local dataset GDPDATA
+function mloadData(GDPDATA){
+	console.log(GDPDATA);
 }
-// 5) Since we are only interested in the observations, we will now pull those out using a variable
 
-/*
- 
- var pullOut = DATASET.observations;
- 
-// 6) We arrange the data we have as an array of arrays
+var mHeader = GDPDATA.columns;
 
- var ideserveArrays = [];
- 
-	//We should also add headers to name our variables
-	
- var myHeader = ["Date", "Value"];
- ideserveArrays.push(myHeader);	
+//4) I will now add my datatable to the visualization using the Google Viz API
 
-// 7) After making the empty parent array, we create a for loop
-// Since there are too many data points, we use the .length property to specify number of loops
 
-for (var i=0; i<pullOut.length; i++) {
-// 8) Within this for loop, we create our second array, i.e. the child array
-	
-	//Observations is an array of objects, so this next variable will loop through each of them
-	var iObject = pullOut[i];
-	
-    //For the chart, we only need two variables - date and value
-    //We need to specify that value is a number, because it is a string in the data set
-    
-    var udeserveArrays = [iObject.date, Number(iObject.value)];
+var gTable = new google.visualization.DataTable();
+// The first parameter is the datatype in the column, the second is the name of the column
+data.addColumn('string', 'DATE');
+data.addColumn('number', 'VALUE');
+data.addRows(GDPDATA.rows);
 
-// 9) We now push our second array into its parent array
-
-	ideserveArrays.push(udeserveArrays);
-	
-	}
-	
-	  
-	console.log(ideserveArrays);
-// 10) Next, I will feed the data into the Google Visualization library using the array to data table function
+/* 
 
 	var dataHere = google.visualization.arrayToDataTable(ideserveArrays);
 
-// 11) We must put the chart on our page using a div, which I will go create in the HTML file now	
  	
  	var makeChart = new google.visualization.BarChart(document.getElementById("chartHere"));
  	
-// 12) Finally, we render the chart on the page, but not before determining what we want it to look like using the 'options' tag
 
 	var options = {
 		title:"Unemployment Trends", curveType:"function", backgroundColor:"white"
 	};
 	
 	makeChart.draw(dataHere, options);
-}
 
 */
-// 3) Now, we define the callback function for the Google Visualization Library
 
-function stepTwo(){
+function gloadData(){
 	console.log ("Google has come to the party");
 	
-// 4) We load our data within this function using .get
-// I will be safe and use the same data set from class.
+//2) Load the Google visualization library from Google Fusion tables using an SQL query
+// We replace the JSON filename in our .get function with the dataset URL
 
-$.get("https://www.googleapis.com/fusiontables/v1/query/?sql=SELECT+*+FROM+153u1bOCk0rHRiH7hMshhuNqv_rozzMnMvn_2hRyc&key=AIzaSyCzc9XKi4CG-PcqBZfHBmc3fKmuq3JH9vU", stepThree, "json");
+$.get("https://www.googleapis.com/fusiontables/v1/query/?sql=SELECT+*+FROM+153u1bOCk0rHRiH7hMshhuNqv_rozzMnMvn_2hRyc&key=AIzaSyCzc9XKi4CG-PcqBZfHBmc3fKmuq3JH9vU", mloadData , "json");
 
-//The .get includes file name, function name and file type
 }
 
-// 2) Next, we define the callback function for document ready
 
-function stepOne(){
-//Console log to make sure it is working
+function mloadPage(){
+
 console.log("My document must be ready");
 
 
-google.load("visualization", "1", {packages:["corechart"], callback:"stepTwo"});
+google.load("visualization", "1", {packages:["corechart"], callback:"gloadData"});
 }
 
-
-$(document).ready(stepOne);
+//1) We are modifying the existing code from the earlier assignment
+$(document).ready(mloadPage);
