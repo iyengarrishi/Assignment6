bc0d85f3082561cbd5935f4c51389772d80839db
Rishi <ri2195@columbia.edu>
2014-03-07 22:41:25 -0500

    Solved the problem and drew the chart. Stupid curly brace.

--- scripts.js
+++ scripts.js
@@ -17,41 +17,32 @@
 
 // 3) Since I am using the GDP dataset instead of UNEMP, I will name my local dataset GDPDATA
 function mloadData(GDPDATA){
-	console.log(GDPDATA);
-}
+console.log(GDPDATA);
 
 //4) I will now add my datatable to the visualization using the Google Viz API
-
 var gTable = new google.visualization.DataTable();
 gTable.addColumn('string', GDPDATA.columns[0]);
 gTable.addColumn('number', GDPDATA.columns[1]);
 gTable.addRows(GDPDATA.rows);
-
-
-/*
-	var dataHere = google.visualization.arrayToDataTable(ideserveArrays);
-
- 	
- 	var makeChart = new google.visualization.BarChart(document.getElementById("chartHere"));
- 	
-
-	var options = {
-		title:"Unemployment Trends", curveType:"function", backgroundColor:"white"
-	};
 	
-	makeChart.draw(dataHere, options);
+//5) Now I will create the chart, and tell the program to put the chart in my div	
+var gChart = new google.visualization.LineChart(document.getElementById("gChartDiv"));
+//Options tag
+var gchartOptions = {
+          title: "GDP Growth", 
+        };
+//6) And finally, we draw the chart
+gChart.draw(gTable, gchartOptions);
 
-*/
-
-function gloadData(){
-	console.log ("Google has come to the party");
-	
+}	
 //2) Load the Google visualization library from Google Fusion tables using an SQL query
 // We replace the JSON filename in our .get function with the dataset URL
-
+function gloadData(){
+	
 $.get("https://www.googleapis.com/fusiontables/v1/query/?sql=SELECT+*+FROM+153u1bOCk0rHRiH7hMshhuNqv_rozzMnMvn_2hRyc&key=AIzaSyCzc9XKi4CG-PcqBZfHBmc3fKmuq3JH9vU", mloadData , "json");
 
 }
+
 
 
 function mloadPage(){
