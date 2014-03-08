7829a68799cc5f124c213a0853337982d5528d50
Rishi <ri2195@columbia.edu>
2014-03-07 20:39:36 -0500

    Loaded data from GoogleFusion Table

--- scripts.js
+++ scripts.js
@@ -3,25 +3,26 @@
  */
 
 /* Project Plan 
-1. Load libraries (jQuery and Google JS)
-2. Setup document.ready function
-3. Load Google Visualization Library and create a callback function
-4. Add data from external JSON file to callback function using .get function
-5. Create an array of arrays of the data, using a for loop
+1. Load jQuery and Google JS libraries
+2. Set up document.ready function
+3. Load Google Visualization Library and create a callback function (Note: These 3 steps can be done by copying earlier script)
+4. Add data from Google Fusion table using SQL query
+5. Filter the data using a WHERE function in the SQL
 6. Feed data to visualization library
 7. Create a div in HTML file for visualization
-8. Create a chart using the Google Viz library set the newly created div as its location
+8. Create a chart using the Google Viz library and set the newly created div as its location
 9. Render the chart
 */
 
-console.log ("Loaded Page");
 
-// 4) Let us set UNEMPLOYMENT as the local filename of the JSON file we just loaded. 
 
 function stepThree(DATASET){
 	console.log(DATASET);
+}
 // 5) Since we are only interested in the observations, we will now pull those out using a variable
 
+/*
+ 
  var pullOut = DATASET.observations;
  
 // 6) We arrange the data we have as an array of arrays
@@ -72,6 +73,7 @@ for (var i=0; i<pullOut.length; i++) {
 	makeChart.draw(dataHere, options);
 }
 
+*/
 // 3) Now, we define the callback function for the Google Visualization Library
 
 function stepTwo(){
@@ -80,7 +82,7 @@ function stepTwo(){
 // 4) We load our data within this function using .get
 // I will be safe and use the same data set from class.
 
-$.get("UEMP270V_data.json", stepThree, "json");
+$.get("https://www.googleapis.com/fusiontables/v1/query/?sql=SELECT+*+FROM+153u1bOCk0rHRiH7hMshhuNqv_rozzMnMvn_2hRyc&key=AIzaSyCzc9XKi4CG-PcqBZfHBmc3fKmuq3JH9vU", stepThree, "json");
 
 //The .get includes file name, function name and file type
 }
@@ -91,14 +93,9 @@ function stepOne(){
 //Console log to make sure it is working
 console.log("My document must be ready");
 
-//Load Google Visualization library
+
 google.load("visualization", "1", {packages:["corechart"], callback:"stepTwo"});
 }
 
-
-
-
-
-// 1) First, we set up document ready
 
 $(document).ready(stepOne);
